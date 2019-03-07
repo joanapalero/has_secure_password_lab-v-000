41270e5100191d6844b13e19229169f376dd2656
Joana Palero <joana.palero24@gmail.com>
2019-03-07 01:31:29 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20190307010456) do
 
-  create_table "create_users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
